a9745b7b144125501390849cdb820533b48c1479
quangkhai88 <canhcung88@gmail.com>
2024-03-22 00:34:54 +0100

    test

--- src/test/java/employee/AdultEmployeePredicateTest.java
+++ src/test/java/employee/AdultEmployeePredicateTest.java
@@ -1,6 +1,8 @@
 package employee;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -13,5 +15,11 @@ class AdultEmployeePredicateTest {
     void test1() {
         assertFalse(predicate.test(new Employee("abc", 15)));
         assertTrue(predicate.test(new Employee("abc", 25)));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {16, 17,2,3,10,12,13,15,17})
+    public void employeeAdultFalse(int age) {
+        assertFalse(predicate.test(new Employee("abc", age)));
     }
 }
