fad5a0ffc100cceec3f0993b5a7583296d2fcb6f
TRAN Quang Khai <canhcung88@gmail.com>
2025-01-07 16:57:07 +0100

    refact with Yatzy1 continue

--- src/main/java/org/codingdojo/yatzy1/Yatzy1.java
+++ src/main/java/org/codingdojo/yatzy1/Yatzy1.java
@@ -1,5 +1,7 @@
 package org.codingdojo.yatzy1;
 
+import java.util.Arrays;
+
 public class Yatzy1 {
 
     public static int chance(int... dice) {
@@ -24,16 +26,8 @@ public class Yatzy1 {
         return 0;
     }
 
-    public static int ones(int d1, int d2, int d3, int d4, int d5) {
-        int sum = 0;
-        if (d1 == 1) sum++;
-        if (d2 == 1) sum++;
-        if (d3 == 1) sum++;
-        if (d4 == 1) sum++;
-        if (d5 == 1) 
-            sum++;
-
-        return sum;
+    public static int ones(int...dices) {
+        return Arrays.stream(dices).filter(dice -> dice == 1).sum();
     }
 
     public static int twos(int d1, int d2, int d3, int d4, int d5) {
