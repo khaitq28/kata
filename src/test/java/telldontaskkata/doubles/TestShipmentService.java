fae64562ae284434f5fd0e3bafa035a4d0440a4b
TRAN Quang Khai <canhcung88@gmail.com>
2025-01-10 17:08:36 +0100

    more

--- src/test/java/telldontaskkata/doubles/TestShipmentService.java
+++ src/test/java/telldontaskkata/doubles/TestShipmentService.java
@@ -1,14 +1,12 @@
 package telldontaskkata.doubles;
 
+import lombok.Getter;
 import telldontaskkata.domain.Order;
 import telldontaskkata.service.ShipmentService;
 
+@Getter
 public class TestShipmentService implements ShipmentService {
     private Order shippedOrder = null;
-
-    public Order getShippedOrder() {
-        return shippedOrder;
-    }
 
     @Override
     public void ship(Order order) {
