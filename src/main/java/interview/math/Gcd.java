8e0e36d90ad2c0a6a2f90146866df15d1b48fcbe
TRAN Quang Khai <canhcung88@gmail.com>
2026-06-07 21:45:21 +0200

    add more

--- src/main/java/interview/math/Gcd.java
+++ src/main/java/interview/math/Gcd.java
@@ -1,5 +1,8 @@
 package interview.math;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Gcd {
 
     public static void main(String[] args) {
@@ -9,6 +12,8 @@ public class Gcd {
         System.out.println("gcd(7,10) = " + gcd(7, 10));
 
         System.out.println("lcm(22,15) = " + lcm(22, 15));
+
+        System.out.println(primeFactor(45));
     }
 
     private static int gcd(int a, int b) {
@@ -27,5 +32,19 @@ public class Gcd {
         for (int i = m; i < a * b; i+= m)
             if (i % Math.min(a,b) == 0) return i;
         return a * b;
+    }
+
+    // 6
+    private static List<Integer> primeFactor(int n) {
+        List<Integer> ret = new ArrayList<>();
+        int k = 2;
+        while (n > 1) {
+            while (n % k == 0) {
+                ret.add(k);
+                n = n / k;
+            }
+            k++;
+        }
+        return ret;
     }
 }
